4369903bf09e69f452540e91c4456c8b6993673e
Tejas Ambhorer <TejasAm2020@gmail.com>
2021-08-29 19:12:12 +0530

    data links

--- src/Home.js
+++ src/Home.js
@@ -51,7 +51,7 @@ const Home = () => {
    //     }, 1000);
    // },[]);
 
-   const { data: blogs, isPending, error } = useFetch('http://TejasAmbhore.github.io/React_two/data/db.json')
+   const { data: blogs, isPending, error } = useFetch('http://TejasAmbhore.github.io/React_two/data/db.json/blogs')
 
     return (
         /*<div className="home">
